61f3559a60ca71f0f2d8e44ecd13dbc1256163f1
piperkfb <piperkfb@gmail.com>
2024-02-21 14:30:40 -0600

    added movement

--- unity-assets_models_textures/Assets/Scripts/PlayerController.cs
+++ unity-assets_models_textures/Assets/Scripts/PlayerController.cs
@@ -4,7 +4,9 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public 
+    public float walkSpeed = 2000f;
+    public float jumpSpeed = 7f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -15,5 +17,32 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         
+    }
+      void playermove()
+    {
+        if (Input.GetKey("w"))
+        {
+            rb.AddForce(0, 0, walkSpeed * Time.deltaTime);
+        }
+
+        if (Input.GetKey("s"))
+        {
+            rb.AddForce(0, 0, -walkSpeed * Time.deltaTime);
+        }
+
+        if (Input.GetKey("a"))
+        {
+            rb.AddForce(-walkSpeed * Time.deltaTime, 0, 0);
+        }
+        
+        if (Input.GetKey("d"))
+        {
+            rb.AddForce(walkSpeed * Time.deltaTime, 0, 0);
+        }
+        if (Input.GetKey("space"))
+        /// make a clause that if not on ground, dont jump
+        {
+            rb.AddForce(0, jumpSpeed * Time.deltaTime, 0)
+        }
     }
 }
