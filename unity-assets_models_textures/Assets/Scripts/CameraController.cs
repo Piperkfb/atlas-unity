64c8562bfe557cb119dfa52cc46c604ed0c8002b
Piperkfb <piperkfb@gmail.com>
2024-02-23 20:45:05 -0600

    timer added, gave up on camera rotation

--- unity-assets_models_textures/Assets/Scripts/CameraController.cs
+++ unity-assets_models_textures/Assets/Scripts/CameraController.cs
@@ -5,17 +5,28 @@ using UnityEngine;
 public class CameraController : MonoBehaviour
 {
     private Vector3 offset;
+    public Transform the;
     public GameObject player;
     public float MouseSpeed = 4f;
     // Start is called before the first frame update
     void Start()
     {
-        
+        the = GetComponent<Transform>();
+       offset = the.position - player.transform.position; 
     }
 
     // Update is called once per frame
-    void Update()
+    private void LateUpdate()
     {
-        
+        if (Input.GetMouseButton(1))
+        {
+            offset = Quaternion.AngleAxis(Input.GetAxis("Mouse X") * MouseSpeed, Vector3.up) * Quaternion.AngleAxis(Input.GetAxis("Mouse Y") * MouseSpeed, Vector3.left) * offset;
+            transform.position = player.transform.position + offset;
+            transform.LookAt(player.transform.position);
+        }
+        else
+        {
+            transform.position = player.transform.position + offset;
+        }
     }
 }
--- unity-assets_models_textures/Assets/Scripts/PlayerController.cs
+++ unity-assets_models_textures/Assets/Scripts/PlayerController.cs
@@ -10,20 +10,27 @@ public class PlayerController : MonoBehaviour
     public float gravity = 9.8f;
     public float fallVelocity;
     private CharacterController player;
+    private Transform playerPos;
+    public Camera mainCamera;
+    private Vector3 camForward;
+    private Vector3 camRight;
     private Vector3 direction = Vector3.zero;
 
     // Start is called before the first frame update
     void Start()
     {
         player = GetComponent<CharacterController>();
+        playerPos = GetComponent<Transform>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        
         if (player.isGrounded)
         {
             direction = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+            direction = transform.TransformDirection(direction);
             direction *= walkSpeed;
             if(Input.GetButton("Jump"))
             {
@@ -33,6 +40,7 @@ public class PlayerController : MonoBehaviour
         else
         {
             direction = new Vector3(Input.GetAxis("Horizontal"), direction.y, Input.GetAxis("Vertical"));
+            direction = transform.TransformDirection(direction);
             direction.x *= walkSpeed;
             direction.z *= walkSpeed;
         }
@@ -40,5 +48,14 @@ public class PlayerController : MonoBehaviour
         player.Move(direction * Time.deltaTime);
         
 
+    }
+    public void CamControl()
+    {
+        camForward = mainCamera.transform.forward;
+        camRight = mainCamera.transform.right;
+        camForward.y = 0;
+        camRight.y =0;
+        camForward = camForward.normalized;
+        camRight = camRight.normalized;
     }
 }
